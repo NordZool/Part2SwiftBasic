2cc9f6f94ec7bea6cd208380f1f49890dc7f520a
Nikita <volodko.nikita.1979@gmail.com>
2023-03-05 16:04:54 +0300

    Add Learn Unsigned int and .min

--- Part2.playground/Contents.swift
+++ Part2.playground/Contents.swift
@@ -1,3 +1,13 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+var poliglot: Int = -35
+
+var UnsignedPoliglot: UInt = 35
+
+
+var clownInt8 = Int8.min
+
+var clownUnInt16Min = UInt16.min
+var clownUnInt16Max = UInt16.max
