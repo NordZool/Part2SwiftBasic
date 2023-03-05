51785442855711982a8600d64200ed2c9bd4d606
Nikita <volodko.nikita.1979@gmail.com>
2023-03-06 00:06:41 +0300

    operations between different data

--- Part2.playground/Contents.swift
+++ Part2.playground/Contents.swift
@@ -2,12 +2,26 @@ import UIKit
 
 var greeting = "Hello, playground"
 
-var poliglot: Int = -35
+
 
 var UnsignedPoliglot: UInt = 35
 
 
+
 var clownInt8 = Int8.min
 
-var clownUnInt16Min = UInt16.min
-var clownUnInt16Max = UInt16.max
+var clowns = Double(23.20)
+var poliglot: Int = -35
+
+
+var all = clowns * Double(poliglot)
+
+var notAll = Int(clowns) * poliglot
+
+
+var str: String = "Give "
+
+var numb: Int = 5
+
+var GiveFive = str + String(numb)
+print(GiveFive)
